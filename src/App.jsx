27adb8b45ca563b01458375cd0b7236cb749beb0
kimtaejin3 <rlaxowls1316@naver.com>
2024-08-04 16:02:46 +0900

    feat: page router 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  ArticlePage,
+  EditorPage,
+  HomePage,
+  LoginPage,
+  ProfilePage,
+  RegisterPage,
+  SettingsPage,
+} from "./pages";
 
 const App = () => {
-  return <div>Hello, world!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/editor" element={<EditorPage />} />
+        <Route path="/article" element={<ArticlePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
