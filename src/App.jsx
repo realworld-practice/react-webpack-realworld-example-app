ff87bfcc496d92b12b8a9c3b5c471fde534e5b35
kimtaejin3 <rlaxowls1316@naver.com>
2024-08-04 15:54:30 +0900

    feat: pages 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -3,4 +3,5 @@ import React from "react";
 const App = () => {
   return <div>Hello, world!</div>;
 };
+
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as ArticlePage } from "./ArticlePage";
+export { default as HomePage } from "./HomePage";
+export { default as EditorPage } from "./EditorPage";
+export { default as LoginPage } from "./LoginPage";
+export { default as ProfilePage } from "./ProfilePage";
+export { default as RegisterPage } from "./RegisterPage";
+export { default as SettingsPage } from "./SettingsPage";
