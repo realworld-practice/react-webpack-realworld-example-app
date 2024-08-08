84cd575a551aa0a5ea93034765519f19ad0ce5d9
kimtaejin3 <rlaxowls1316@naver.com>
2024-08-08 13:37:50 +0900

    feat: Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,23 @@ import {
   RegisterPage,
   SettingsPage,
 } from "./pages";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Layout from "./layouts/Layout";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/editor" element={<EditorPage />} />
-        <Route path="/article" element={<ArticlePage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/editor" element={<EditorPage />} />
+          <Route path="/article" element={<ArticlePage />} />
+        </Route>
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 };
